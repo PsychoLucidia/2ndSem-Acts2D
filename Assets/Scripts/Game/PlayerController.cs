e44434ae81e3f18b5b21640013957775d2c0c5cb
PsychoLucidia <89222930+PsychoLucidia@users.noreply.github.com>
2024-01-25 11:57:19 +0800

    Added Script

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,18 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float BoundY;
+
+    public Rigidbody2D playerPanel;
+
+    public int wcPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPanel = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var panelVelocity = playerPanel.velocity;
+
+        if (wcPlayer == 1)
+        {
+            if (Input.GetKey(KeyCode.W)) //W is pressed
+            {
+                panelVelocity.y = speed;
+            }
+
+            else if (Input.GetKey(KeyCode.S)) //S is pressed
+            {
+                panelVelocity.y = -speed;
+            }
+            else 
+            {
+                panelVelocity.y = 0; //No input
+            }
+        }
+
+        if (wcPlayer == 2)
+        {
+            if (Input.GetKey(KeyCode.UpArrow)) //Up is pressed
+            {
+                panelVelocity.y = speed;
+            }
+
+            else if (Input.GetKey(KeyCode.DownArrow)) //Down is pressed
+            {
+                panelVelocity.y = -speed;
+            }
+            else
+            {
+                panelVelocity.y = 0; //No input
+            }
+        }
+
+        playerPanel.velocity = panelVelocity;
+
+        var panelPos = transform.position;
+
+        if (panelPos.y > BoundY)
+        {
+            panelPos.y = BoundY;
+        }
+        else if (panelPos.y < -BoundY)
+        {
+            panelPos.y = -BoundY;
+        }
+        transform.position = panelPos;
     }
 }
